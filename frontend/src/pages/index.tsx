6acc0103b1ddf05638c7c7bd8a8e6bc23777de8a
ajay <ajayraj98@gmail.com>
2025-02-26 11:37:16 -0800

    git commit -m "Added OAuth authentication with NextAuth.js, resolves #30, updates #13, updates #23"

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function HomePage() {
+  const { data: session } = useSession();
+
+  return (
+    <div>
+      {session ? (
+        <>
+          <p>Welcome, {session.user?.name}!</p>
+          <button onClick={() => signOut()}>Logout</button>
+        </>
+      ) : (
+        <button onClick={() => signIn("google")}>Sign in with Google</button>
+      )}
+    </div>
+  );
+}
